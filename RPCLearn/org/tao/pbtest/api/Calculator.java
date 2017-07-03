a1fdc2fdace481afed6c01a5a8d60d5a184686c4
wayblink <137658599@qq.com>
2017-07-03 23:16:05 +0800

    delete RPCLearn

--- RPCLearn/org/tao/pbtest/api/Calculator.java
+++
@@ -1,6 +0,0 @@
-package org.tao.pbtest.api;  
-      
-public interface Calculator {  
-   public int add(int a, int b);  
-   public int minus(int a, int b);  
-}  
--- RPCLearn/org/tao/pbtest/api/CalculatorPB.java
+++
@@ -1,6 +0,0 @@
-package org.tao.pbtest.server.api;  
-      
-import org.tao.pbtest.proto.Calculator.CalculatorService.BlockingService;  
-      
-public interface CalculatorPB extends BlockingInterface {  
-}  
