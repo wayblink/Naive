2f807aecb991deefb7545cd6eee80ea782b2557b
wayblink <137658599@qq.com>
2017-07-03 23:14:07 +0800

    create dir basic to store things related to basic tools and knoeledge

---
+++ basic/RPCLearn/org/tao/pbtest/api/CalculatorPB.java
@@ -0,0 +1,6 @@
+package org.tao.pbtest.server.api;  
+      
+import org.tao.pbtest.proto.Calculator.CalculatorService.BlockingService;  
+      
+public interface CalculatorPB extends BlockingInterface {  
+}  
