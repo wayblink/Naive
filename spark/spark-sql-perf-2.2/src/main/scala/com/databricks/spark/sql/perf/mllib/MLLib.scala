69c2679ec2b2c085799d5404c87d3557be5cd1a1
wanganyang <wanganyang@bytedance.com>
2018-04-18 14:51:42 +0800

    spark-sql-perf-2.2

---
+++ spark/spark-sql-perf-2.2/src/main/scala/com/databricks/spark/sql/perf/mllib/MLLib.scala
@@ -0,0 +1,82 @@
+package com.databricks.spark.sql.perf.mllib
+
+import scala.language.implicitConversions
+
+import com.typesafe.scalalogging.slf4j.{LazyLogging => Logging}
+
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import com.databricks.spark.sql.perf._
+
+
+class MLLib(sqlContext: SQLContext)
+  extends Benchmark(sqlContext) with Serializable {
+
+  def this() = this(SQLContext.getOrCreate(SparkContext.getOrCreate()))
+}
+
+object MLLib extends Logging {
+
+  /**
+   * Runs a set of preprogrammed experiments and blocks on completion.
+   *
+   * @param runConfig a configuration that is av
+   * @return
+   */
+  def runDefault(runConfig: RunConfig): DataFrame = {
+    val ml = new MLLib()
+    val benchmarks = MLBenchmarks.benchmarkObjects
+    val e = ml.runExperiment(
+      executionsToRun = benchmarks)
+    e.waitForFinish(1000 * 60 * 30)
+    logger.info("Run finished")
+    e.getCurrentResults()
+  }
+
+  /**
+   * Entry point for running ML tests. Expects a single command-line argument: the path to
+   * a YAML config file specifying which ML tests to run and their parameters.
+   * @param args command line args
+   */
+  def main(args: Array[String]): Unit = {
+    val configFile = args(0)
+    run(yamlFile = configFile)
+  }
+
+  /**
+   * Runs all the experiments and blocks on completion
+   *
+   * @param yamlFile a file name
+   * @return
+   */
+  def run(yamlFile: String = null, yamlConfig: String = null): DataFrame = {
+    logger.info("Starting run")
+    val conf: YamlConfig = Option(yamlFile).map(YamlConfig.readFile).getOrElse {
+      require(yamlConfig != null)
+      YamlConfig.readString(yamlConfig)
+    }
+
+    val sparkConf = new SparkConf().setAppName("MLlib QA").setMaster("local[2]")
+    val sc = SparkContext.getOrCreate(sparkConf)
+    sc.setLogLevel("INFO")
+    val b = new com.databricks.spark.sql.perf.mllib.MLLib()
+    val sqlContext = com.databricks.spark.sql.perf.mllib.MLBenchmarks.sqlContext
+    val benchmarksDescriptions = conf.runnableBenchmarks
+    val benchmarks = benchmarksDescriptions.map { mlb =>
+      new MLPipelineStageBenchmarkable(mlb.params, mlb.benchmark, sqlContext)
+    }
+    println(s"${benchmarks.size} benchmarks identified:")
+    val str = benchmarks.map(_.prettyPrint).mkString("\n")
+    println(str)
+    logger.info("Starting experiments")
+    val e = b.runExperiment(
+      executionsToRun = benchmarks,
+      iterations = 1, // If you want to increase the number of iterations, add more seeds
+      resultLocation = conf.output,
+      forkThread = false)
+    e.waitForFinish(conf.timeout.toSeconds.toInt)
+    logger.info("Run finished")
+    e.getCurrentResults()
+  }
+}
---
+++ spark/spark-sql-perf-2.2/src/main/scala/com/databricks/spark/sql/perf/mllib/MLPipelineStageBenchmarkable.scala
@@ -0,0 +1,134 @@
+package com.databricks.spark.sql.perf.mllib
+
+import scala.collection.mutable.ArrayBuffer
+
+import com.typesafe.scalalogging.slf4j.{LazyLogging => Logging}
+
+import org.apache.spark.ml.{Estimator, Transformer}
+import org.apache.spark.sql._
+import org.apache.spark.{SparkContext, SparkEnv}
+
+import com.databricks.spark.sql.perf._
+
+class MLPipelineStageBenchmarkable(
+    params: MLParams,
+    test: BenchmarkAlgorithm,
+    sqlContext: SQLContext)
+  extends Benchmarkable with Serializable with Logging {
+
+  import MLPipelineStageBenchmarkable._
+
+  private var testData: DataFrame = null
+  private var trainingData: DataFrame = null
+  private var testDataCount: Option[Long] = None
+  private val param = MLBenchContext(params, sqlContext)
+
+  override val name = test.name
+
+  override protected val executionMode: ExecutionMode = ExecutionMode.SparkPerfResults
+
+  override protected def beforeBenchmark(): Unit = {
+    logger.info(s"$this beforeBenchmark")
+    try {
+      testData = test.testDataSet(param)
+      testData.cache()
+      testDataCount = Some(testData.count())
+      trainingData = test.trainingDataSet(param)
+      trainingData.cache()
+      trainingData.count()
+    } catch {
+      case e: Throwable =>
+        println(s"$this error in beforeBenchmark: ${e.getStackTraceString}")
+        throw e
+    }
+  }
+
+  override protected def afterBenchmark(sc: SparkContext): Unit = {
+    // Best-effort clean up of weakly referenced RDDs, shuffles, and broadcasts
+    // Remove any leftover blocks that still exist
+    sc.getExecutorStorageStatus
+      .flatMap { status => status.blocks.map { case (bid, _) => bid } }
+      .foreach { bid => SparkEnv.get.blockManager.master.removeBlock(bid) }
+    super.afterBenchmark(sc)
+  }
+
+  override protected def doBenchmark(
+    includeBreakdown: Boolean,
+    description: String,
+    messages: ArrayBuffer[String]): BenchmarkResult = {
+    try {
+      val (trainingTime, model: Transformer) = measureTime {
+        logger.info(s"$this: train: trainingSet=${trainingData.schema}")
+        test.getPipelineStage(param) match {
+          case est: Estimator[_] => est.fit(trainingData)
+          case transformer: Transformer =>
+            transformer.transform(trainingData)
+            transformer
+          case other: Any => throw new UnsupportedOperationException("Algorithm to benchmark must" +
+            s" be an estimator or transformer, found ${other.getClass} instead.")
+        }
+      }
+      logger.info(s"model: $model")
+      val (scoreTrainTime, scoreTraining) = measureTime {
+        test.score(param, trainingData, model)
+      }
+      val (scoreTestTime, scoreTest) = measureTime {
+        test.score(param, testData, model)
+      }
+
+      logger.info(s"$this doBenchmark: Trained model in ${trainingTime.toMillis / 1000.0}" +
+        s" s, Scored training dataset in ${scoreTrainTime.toMillis / 1000.0} s," +
+        s" test dataset in ${scoreTestTime.toMillis / 1000.0} s")
+
+
+      val ml = MLResult(
+        trainingTime = Some(trainingTime.toMillis),
+        trainingMetric = Some(scoreTraining),
+        testTime = Some(scoreTestTime.toMillis),
+        testMetric = Some(scoreTest / testDataCount.get))
+
+      BenchmarkResult(
+        name = name,
+        mode = executionMode.toString,
+        parameters = params.toMap,
+        executionTime = Some(trainingTime.toMillis),
+        mlResult = Some(ml))
+    } catch {
+      case e: Exception =>
+        BenchmarkResult(
+          name = name,
+          mode = executionMode.toString,
+          parameters = params.toMap,
+          failure = Some(Failure(e.getClass.getSimpleName,
+            e.getMessage + ":\n" + e.getStackTraceString)))
+    } finally {
+      Option(testData).map(_.unpersist())
+      Option(trainingData).map(_.unpersist())
+    }
+  }
+
+  def prettyPrint: String = {
+    val paramString = pprint(params).mkString("\n")
+    s"$test\n$paramString"
+  }
+
+
+}
+
+object MLPipelineStageBenchmarkable {
+  private def pprint(p: AnyRef): Seq[String] = {
+    val m = getCCParams(p)
+    m.flatMap {
+      case (key, Some(value: Any)) => Some(s"  $key=$value")
+      case _ => None
+    } .toSeq
+  }
+
+  // From http://stackoverflow.com/questions/1226555/case-class-to-map-in-scala
+  private def getCCParams(cc: AnyRef) =
+    (Map[String, Any]() /: cc.getClass.getDeclaredFields) {(a, f) =>
+      f.setAccessible(true)
+      a + (f.getName -> f.get(cc))
+    }
+}
+
